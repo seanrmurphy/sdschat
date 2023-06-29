10a7a779aedbaff2b89cde73c4d1bd890f75df97
Carlos Vivar Rios <carlosvivarrios@gmail.com>
2023-06-29 08:35:37 +0200

    implemented models in env variable

--- Dockerfile
+++ Dockerfile
@@ -17,6 +17,22 @@ RUN apt-get update && apt-get install -y \
 
 RUN ln -s /usr/bin/python3 /usr/bin/python
 
+#RUN useradd -ms /bin/bash sdsc
+#USER sdsc
+#WORKDIR /home/sdsc
+
+# ARG UID=10001
+# RUN adduser \
+#     --disabled-password \
+#     --gecos "" \
+#     --home "/home/sdsc" \
+#     --shell "/bin/bash" \
+#     --uid "${UID}" \
+#     sdsc
+
+# USER sdsc
+WORKDIR /home/sdsc
+
 # Torch dependencies
 RUN pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu117
 
@@ -31,11 +47,13 @@ RUN pip install transformers \
     langchain \
     nvitop
 
+COPY ./app /home/sdsc/app
 
-COPY ./app /app
-WORKDIR /app
+# USER root
+# RUN chmod 777 /home/sdsc/app/entrypoint.sh
+# USER sdsc
 
 # start jupyter lab
-CMD ["bash","entrypoint.sh"]
+CMD ["bash","/home/sdsc/app/entrypoint.sh"]
 EXPOSE 8000
 EXPOSE 7860
--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -25,9 +25,13 @@
 # GPU.
 
 #MODEL="lmsys/vicuna-7b-v1.3"
-MODEL="nomic-ai/gpt4all-13b-snoozy"
-MODELNAME="$(cut -d'/' -f2 <<< $MODEL)"
-export LOGDIR="/tmp/fastchat"
+#MODEL="nomic-ai/gpt4all-13b-snoozy"
+#MODEL="lmsys/fastchat-t5-3b-v1.0"
+#MODEL="tiiuae/falcon-40b"
+
+# Models should be declared in env variable
+#MODELNAME="$(cut -d'/' -f2 <<< $MODEL)"
+export LOGDIR="/home/sdsc/fastchat"
 
 mkdir -p $LOGDIR
 cd $LOGDIR
@@ -35,13 +39,11 @@ cd $LOGDIR
 echo
 echo "Running controller..."
 python3 -m fastchat.serve.controller --host 0.0.0.0 >> controller.txt 2>&1 &
-#tmux new-session -d -s controller 'python3 -m fastchat.serve.controller --host 0.0.0.0'
 
 sleep 5 
 echo
 echo "Running OpenAI API server on port 8000..."
 python3 -m fastchat.serve.openai_api_server --host 0.0.0.0 --port 8000 >> openai_api_server.txt 2>&1 &
-#tmux new-session -d -s openai_api_server 'python3 -m fastchat.serve.openai_api_server --host 0.0.0.0 --port 8000'
 
 # The model names specified below have two purposes:
 # - to identify themselves when they receive API calls
@@ -51,7 +53,46 @@ python3 -m fastchat.serve.openai_api_server --host 0.0.0.0 --port 8000 >> openai
 sleep 5
 echo
 echo "Running model server for $MODEL..."
-python3 -m fastchat.serve.model_worker --model-names "gpt-3.5-turbo,text-davinci-003,text-embedding-ada-002,$MODELNAME" --model-path $MODEL --host 0.0.0.0 >> model_worker.txt 2>&1 &
+
+IFS=',' read -ra MODELS_ARRAY <<< "$MODELS"
+totalModels=0
+#Print the split string
+for MODEL in "${MODELS_ARRAY[@]}"
+do
+    echo "Running model server for $MODEL..."
+    MODELNAME="$(cut -d'/' -f2 <<< $MODEL)"
+    python3 -m fastchat.serve.model_worker --model-names "$MODELNAME" --model-path $MODEL --host 0.0.0.0 >> ${MODELNAME}_worker.txt 2>&1 &
+
+    totalModels=$((totalModels + 1))
+    sleep 1
+done
+
+# Checking if a model for openai interface is requested
+if ! [[ -z "${OPENAIMODEL}" ]]; then
+    echo "$OPENAIMODEL openai model detected."
+    MODELNAME="$(cut -d'/' -f2 <<< $OPENAIMODEL)"
+    python3 -m fastchat.serve.model_worker --model-names "gpt-3.5-turbo,text-davinci-003,text-embedding-ada-002,$MODELNAME" --model-path $OPENAIMODEL --host 0.0.0.0 >> ${MODELNAME}_worker.txt 2>&1 &   
+    totalModels=$((totalModels + 1))
+    sleep 1
+fi
+
+echo "$totalModels detected. Waiting for downloading..."
+loadedModels=0
+while ! [ $loadedModels = $totalModels ]
+do
+    for MODEL in "${MODELS_ARRAY[@]}"
+    do
+        echo "Checking $MODEL model worker..."
+        MODELNAME="$(cut -d'/' -f2 <<< $MODEL)"
+        if cat ${MODELNAME}_worker.txt | grep Uvicorn ; then
+            echo "$MODEL model worker detected."
+            loadedModels=$((loadedModels + 1))
+        fi
+
+        echo $(tail -2 ${MODELNAME}_worker.txt)
+        sleep 2
+    done
+done
 
 
 # The gradio server needs to be started after the model_worker has registered with
@@ -61,20 +102,9 @@ python3 -m fastchat.serve.model_worker --model-names "gpt-3.5-turbo,text-davinci
 # To test if a model has been registered and is usable, this command can be used:
 #   python3 -m fastchat.serve.test_message --model-name vicuna-7b-v1.3
 
-over=False
-while [ $over = False ]
-do
-  echo "Checking model worker..."
-  if cat model_worker.txt | grep Uvicorn ; then
-   echo "Model worker detected."
-   over=True
-   echo "Running Gradio web app server for chat (port 7860 by default) ..."
-   #tmux new-session -d -s gradio 'python3 -m fastchat.serve.gradio_web_server'
-   python3 -m fastchat.serve.gradio_web_server >> gradio_web_server.txt 2>&1 &
-  fi
-  echo $(tail -2 model_worker.txt)
-  sleep 5
-done
+echo "Running Gradio web app server for chat (port 7860 by default) ..."
+#tmux new-session -d -s gradio 'python3 -m fastchat.serve.gradio_web_server'
+python3 -m fastchat.serve.gradio_web_server >> gradio_web_server.txt 2>&1 &
 
 echo "All services started; see log output in $LOGDIR"
 bash
